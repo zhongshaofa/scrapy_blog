8ace63eb6154e2393294b8df4f4ca1591fe912f4
zhongshaofa <2286732552@qq.com>
2018-11-14 02:16:56 +0800

    分页可控爬虫，数据入库

--- scrapy_blog/pipelines.py
+++ scrapy_blog/pipelines.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import pymysql
+from scrapy_blog.settings import MYSQL_HOST, MYSQL_PORT, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DBNAME
+
 
 # Define your item pipelines here
 #
@@ -7,5 +10,32 @@
 
 
 class ScrapyBlogPipeline(object):
+
+    # 初始化mysql服务
+    def __init__(self):
+        self.db = pymysql.connect(MYSQL_HOST, MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_DBNAME)
+        self.cursor = self.db.cursor()
+
+    # 持久化数据到数据库中
     def process_item(self, item, spider):
+
+        # 查询数据库中是否存在该数据
+        query = "SELECT * from article WHERE title='" + item['title'] + "'"
+        self.cursor.execute(query)
+        article = self.cursor.fetchall()
+        if article:
+            return "数据库中已存在该文章:" + item['title']
+
+        # 插入数据库中去
+        insert = "INSERT INTO article " \
+                 "(`author`, `clicks`,  `create_time`, `describe`, `head_img`, `praise`, `title`, `url`, `content`)\
+        VALUES ('" + item['author'] + "', '" + item['clicks'] + "', '" + item['create_time'] \
+                 + "', '" + item['describe'] + "', '" + item['head_img'] + "', '" + item['praise'] + "', '" \
+                 + item['title'] + "', '" + item['url'] + "', '" + item['content'] + "')"
+        try:
+            self.cursor.execute(insert)
+            self.db.commit()
+        except Exception as e:
+            self.db.rollback()
+            return e
         return item
--- scrapy_blog/spiders/cnblogs_spider.py
+++ scrapy_blog/spiders/cnblogs_spider.py
@@ -1,40 +1,42 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
 from scrapy_blog.items import ScrapyBlogItem
 
 
 class CnblogsSpiderSpider(scrapy.Spider):
     name = 'cnblogs_spider'
     allowed_domains = ['www.cnblogs.com']
-    start_urls = ['https://www.cnblogs.com/']
+    start_urls = ['https://www.cnblogs.com']
+    index = 1
+    number = 3
 
+    # 爬取数据
     def parse(self, response):
         list = response.xpath("//div[@id='post_list']//div[@class='post_item']")
         for item in list:
             blog_item = ScrapyBlogItem()
             blog_item['title'] = item.xpath(".//h3/a/text()").extract_first()
             blog_item['url'] = item.xpath(".//h3/a/@href").extract_first()
-            blog_item['describe'] = item.xpath(
-                "normalize-space(.//p[@class='post_item_summary']//text())").extract_first()
+            blog_item['describe'] = item.xpath(".//p[@class='post_item_summary']").extract_first()
             blog_item['head_img'] = item.xpath(".//p/a/img[@class='pfs']/@src").extract_first()
             blog_item['praise'] = item.xpath(".//span[@class='diggnum']//text()").extract_first()
             blog_item['author'] = item.xpath(".//div//a[@class='lightblue']//text()").extract_first()
             blog_item['clicks'] = item.xpath(".//div//span[@class='article_view']//a/text()").extract_first()
+            blog_item['create_time'] = item.xpath(".//div[@class='post_item_foot']").extract_first()
             self.correct_item(blog_item)
-            # blog_item['create_time'] = item.xpath(".//div[@class='post_item_foot']//text()").extract_first()
-            # print(blog_item)
             yield scrapy.Request(url=blog_item['url'], meta={'items': blog_item}, callback=self.parse_content,
                                  dont_filter=True)
+        next_link = response.xpath("//div//div[@class='pager']//a[last()]//@href").extract_first()
+        if not (next_link is None) and self.index < self.number:
+            self.index = self.index + 1
+            yield scrapy.Request(self.start_urls[0] + next_link, callback=self.parse)
 
     # 爬取文章内容
     def parse_content(self, response):
         blog_item = response.meta['items']
         blog_item['content'] = response.xpath("//div//div[@id='cnblogs_post_body']").extract_first()
-        print(blog_item)
-        # items['time'] = response.xpath('//div[@class="jbqk"]/ul/li[5]/text()')[0].extract()
-        # items['detail_content'] = response.xpath('//div[@class="tsnr"]/p/text()')[0].extract()
-        # items['Reply'] = response.xpath('//div[@class="tshf"]/p/text()')[0].extract()
-        # yield items
+        yield blog_item
 
     # 修正数据
     def correct_item(self, items):
@@ -43,4 +45,19 @@ class CnblogsSpiderSpider(scrapy.Spider):
                 items[item] = ''
                 items[item].strip()
         items['head_img'] = "http:" + items['head_img']
+        items['describe'] = self.filter_html_tag(items['describe'])
+        items['create_time'] = self.get_create_time(items['create_time'])
         return items
+
+    # 替换标签
+    def filter_html_tag(self, html):
+        rule = re.compile(r'<[^>]+>', re.S)
+        html = rule.sub('', html)
+        return html.strip()
+
+    # 获取创建时间
+    def get_create_time(self, time):
+        rule = '发布于'
+        location = time.index(rule)
+        time = time[location + 4:location + 20]
+        return time
